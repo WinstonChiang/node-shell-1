1233982e02089f8b30c5719b65c2c55ce63c0623
Winston Chiang <winstonchiang06@gmail.com>
2020-08-07 14:48:47 -0400

    struggle bus on ls

--- bash.js
+++ bash.js
@@ -1,15 +1,17 @@
+const ls = require('./ls');
 process.stdout.write('prompt > ');
 
-const pwd = require('./pwd')
+const pwd = require('./pwd');
 
-// console.log(pwd)
+console.log(ls);
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
-    //
-    process.stdout.write(pwd.cwd)
-    process.stdout.write('\nprompt > ')
+    process.stdout.write(pwd.cwd);
+    process.stdout.write('\nprompt > ');
+  } else if (cmd === 'ls') {
+    console.log('hello');
   } else {
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
